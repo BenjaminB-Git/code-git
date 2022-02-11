97900e57b064aa8fcfc08e6be7164928ba971379
BenjaminB <benjaminblondel1312@gmail.com>
2022-02-11 12:36:25 +0100

    Début JS

---
+++ Code/JS/Tests/script.js
@@ -0,0 +1,7 @@
+var a = window.prompt("Entrez un premier nombre")
+var b = window.prompt("Entrer un deuxième nombre")
+var c = window.prompt("Entrez un opérateur + * - ou /")
+a = number(a)
+b = number(b)
+var add = a+b
+alert(add)
