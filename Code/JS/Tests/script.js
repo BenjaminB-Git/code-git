92b4e3b9be7e23bae4d137f85e8d78b5e26e8d61
BenjaminB-Git <benjaminblondel1312@gmail.com>
2022-02-11 14:47:34 +0100

    Update script.js

--- Code/JS/Tests/script.js
+++ Code/JS/Tests/script.js
@@ -1,7 +1,39 @@
 var a = window.prompt("Entrez un premier nombre")
 var b = window.prompt("Entrer un deuxième nombre")
 var c = window.prompt("Entrez un opérateur + * - ou /")
-a = number(a)
-b = number(b)
-var add = a+b
-alert(add)
+a = Number(a)
+b = Number(b)
+
+switch (c)
+{
+    default:
+        alert("Opérateur invalide");
+        break;
+    case "+":
+        var add = a+b;
+        alert(a + " + " + b + " = " + add);
+        break;
+    case "-":
+        var sub = a-b;
+        alert(a + " - " + b + " = " + sub);
+        break;
+    case "*":
+        var prod = a*b
+        alert(a + " x " + b + " = " + prod);
+        break;
+    case "/":
+        if(b == 0)
+        {
+            alert("Division par zéro impossible");
+        }
+        else
+        {
+            var divide = a/b
+            alert(a + " / " + b + " = " + divide);
+
+        }
+
+
+    
+    
+}
