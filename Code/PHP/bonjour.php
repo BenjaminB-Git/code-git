a260ac1418bd91b3aa5605b5aa0066b585936d54
BenjaminB <benjaminblondel1312@gmail.com>
2022-02-24 17:03:00 +0100

    fonctions PHP

--- Code/PHP/bonjour.php
+++ Code/PHP/bonjour.php
@@ -8,36 +8,43 @@
 </head>
 <body>
     <?php
-    
+
+function lien($link,$title)
+{
+    echo '<a href="'.$link.'">' .$title. '</a>';
+};
+
+lien("https://www.reddit.com/", "Reddit Hug");
 
 
+function somme($tableau)
+{
+    $sum = 0;
+    for ($i = 0; $i < count($tableau); $i++)
+    {
+        $sum += $tableau[$i];
+    };
+    echo $sum;
+};
+
+$tab = array(4, 3, 8, 2);
+$resultat = somme($tab);
+
+$resultat;
 
 
+//Y'a rien qui va ici, à reprendre
+function complex_password($mdp)
+{
+    $success = 'true';
+    if (preg_match('/^(.*[A-Z]+.*)$/gm',$mdp))
+    {
+        $success = 'false';
+    };
+    echo $success;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+complex_password('AaaaAaaaaa');
 
     ?>
     
