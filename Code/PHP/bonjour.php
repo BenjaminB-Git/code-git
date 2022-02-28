adac9f5ec926cd1ab4fbfe905a514c32fbc166c6
BenjaminB <benjaminblondel1312@gmail.com>
2022-02-28 16:52:57 +0100

    PHP suite

--- Code/PHP/bonjour.php
+++ Code/PHP/bonjour.php
@@ -9,42 +9,24 @@
 <body>
     <?php
 
-function lien($link,$title)
-{
-    echo '<a href="'.$link.'">' .$title. '</a>';
-};
-
-lien("https://www.reddit.com/", "Reddit Hug");
+echo "bonjour" . $_REQUEST['prenom'] . " " . $_REQUEST['nom'] . "<br>";
 
 
-function somme($tableau)
-{
-    $sum = 0;
-    for ($i = 0; $i < count($tableau); $i++)
-    {
-        $sum += $tableau[$i];
-    };
-    echo $sum;
-};
-
-$tab = array(4, 3, 8, 2);
-$resultat = somme($tab);
-
-$resultat;
 
 
-//Y'a rien qui va ici, à reprendre
-function complex_password($mdp)
-{
-    $success = 'true';
-    if (preg_match('/^(.*[A-Z]+.*)$/gm',$mdp))
-    {
-        $success = 'false';
-    };
-    echo $success;
-}
 
-complex_password('AaaaAaaaaa');
+
+
+
+
+
+
+
+
+
+
+
+
 
     ?>
     
