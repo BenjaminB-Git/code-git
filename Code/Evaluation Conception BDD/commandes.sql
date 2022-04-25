d22368fecf2954ef8cc8fe4d71d5567f26ceade2
Benjamin <benjaminblondel1312@gmail.com>
2022-04-25 14:04:14 +0200

    Modif évaluation

--- Code/Evaluation Conception BDD/commandes.sql
+++ Code/Evaluation Conception BDD/commandes.sql
@@ -1,29 +1,29 @@
-DELETE DATABASE IF EXISTS commandes;
+DROP DATABASE IF EXISTS commandes;
 
 CREATE DATABASE commandes;
 
 USE commandes;
 
 
-/** Client est remplacé par Client_site afin d'éviter les conflits **/
 CREATE TABLE Client_site(
-    N°Client COUNTER NOT NULL AUTO_INCREMENT,
+    N°Client INT NOT NULL AUTO_INCREMENT,
     NomClient VARCHAR(50),
     PrenomClient VARCHAR(50),
     PRIMARY KEY (N°Client)
 );
 
 CREATE TABLE Article(
-    N°Article COUNTER NOT NULL AUTO_INCREMENT,
+    N°Article INT NOT NULL AUTO_INCREMENT,
     DesignationArticle VARCHAR(255),
     PUArticle DECIMAL(9,2),
     PRIMARY KEY (N°Article)
 );
 
 CREATE TABLE Commande(
-    N°Commande COUNTER NOT NULL AUTO_INCREMENT,
+    N°Commande INT NOT NULL AUTO_INCREMENT,
     DateCommande DATE,
     MontantCommande DECIMAL(9,2),
+    N°Client INT NOT NULL,
     FOREIGN KEY (N°Client) REFERENCES Client_site(N°Client),
     PRIMARY KEY (N°Commande)
 );
@@ -31,6 +31,8 @@ CREATE TABLE Commande(
 CREATE TABLE SeComposeDe(
     Qte INT,
     TauxTva DECIMAL(4,2),
+    N°Article INT NOT NULL,
+    N°Commande INT NOT NULL,
     FOREIGN KEY (N°Article) REFERENCES Article (N°Article),
     FOREIGN KEY (N°Commande) REFERENCES Commande (N°Commande),
     PRIMARY KEY (N°Article, N°Commande)
